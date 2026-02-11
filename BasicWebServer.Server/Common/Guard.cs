5f09ad8683a39520ba48cf4e4ca649d161f32a31
BlagoiMurad <blagoimurad57@gmail.com>
2026-02-11 07:57:03 +0200

    Add project files.

---
+++ BasicWebServer.Server/Common/Guard.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BasicWebServer.Server.Common
+{
+    public static class Guard
+    {
+        public static void AgainstNull(object value, string name = null)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+}
